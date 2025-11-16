403a55c2129d522167fade2c0ec80557ede93f00
Jose Valverde <jval7331_@hotmail.com>
2025-11-15 19:57:44 -0500

    Proyecto Final (Codigo)

---
+++ atlas/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "mainwindow.h"
+
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsRectItem>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QGraphicsScene * scene =  new QGraphicsScene();
+
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect -> setRect(0,0,100,100); //(x, y, width, height)
+
+    scene -> addItem(rect);
+
+    QGraphicsView * view = new QGraphicsView(scene);
+    view -> show();
+
+    return a.exec();
+}
